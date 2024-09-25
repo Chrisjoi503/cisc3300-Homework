69a4ed7839544d1a870313d4d8518eb628807b43
Christina <christina@MacBook-Pro-387.local>
2024-09-25 16:53:24 -0400

    inclass

---
+++ in-class-4.js
@@ -0,0 +1,16 @@
+
+console.log("in-class-4.js loaded!");
+
+const array1 = [1,2,3,4,5];
+
+for( let i=0; i<array1.length+1; i++)
+{
+    if( i%2===0)
+    {
+         console.log(`${i} is even!`)
+    }
+    else 
+    {
+        console.log(`${i} is odd!`)
+    }
+}
